d857f2d2ab9975b750fc2e81f43f125eda23fc5d
Joseph Ho <Josephho678@gmail.com>
2023-04-08 20:23:23 -0400

    Accept requests on endpoint and store payload.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs');
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const express = require('express');
+const { Pool } = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -45,27 +46,71 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
-// Express request things
+// Express request and backend things
 
 const app = express();
 app.use(express.json());
 
+const pg_client = new Pool({
+	user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_DB_NAME,
+	password: process.env.PG_PASS,
+	port: 5432,
+	ssl: true,
+});
+
+// Test the database connection
+pg_client.connect((err, client, done) => {
+	if (err) {
+	  console.error('Error connecting to database', err.stack);
+	} else {
+	  console.log('Connected to database');
+	  client.query('SELECT NOW()', (err, result) => {
+		done();
+		if (err) {
+		  console.error('Error executing query', err.stack);
+		} else {
+		  console.log('Successfully executed query', result.rows[0]);
+		}
+	  });
+	}
+});
+
 
 // End point for github-webhooks. This will take the payload
 // and send it straight into the given discord channel.
-app.post('/github-webhook', (req, res) => {
+app.post('/github-webhook', async (req, res) => {
 	const payload = req.body;
+	const eventType = req.headers['x-github-event'];
 	const webhookChannelID = "888233572949975090";
 	const webhookChannel = client.channels.cache.get(webhookChannelID);
   
-	if (webhookChannel && webhookChannel.type === 'text') {
-	  webhookChannel.send(`Received payload:\n\`\`\`${JSON.stringify(payload)}\`\`\``);
+	if (webhookChannel && webhookChannel.isTextBased()) {
+		if (JSON.stringify(payload).length > 1900) {
+				webhookChannel.send(`Received payload (payload too large, check the database):\n\`\`\`${JSON.stringify(payload).substring(0, 1900)}...\`\`\``);
+		  } else {
+				webhookChannel.send(`Received payload:\n\`\`\`${JSON.stringify(payload)}\`\`\``);
+		  }
 	}
+
+	try {
+		// Insert the event into the database
+		const result = await pg_client.query(`
+		  INSERT INTO github_events (event_type, payload)
+		  VALUES ($1, $2)
+		  RETURNING id
+		`, [eventType, payload]);
+		console.log('Event inserted with ID', result.rows[0].id);
+		res.sendStatus(200);
+	  } catch (err) {
+		console.error('Error inserting event', err.stack);
+		res.sendStatus(500);
+	  }
 	
 	// Backend response to client
 	// TODO: Maybe change this up to better give more info and more 
 	// up to standards.
-	res.send('Received');
 });
   
 client.login(token);
